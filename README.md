a6933dc953263dd5a768ff181069fdb90efa9b11
Shirusu Ryu <shirusuryu@gmail.com>
2026-07-30 21:44:46 +0200

    add doc

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Qwx
 
+## Show manual
+
+```bash
+qwx --help | less
+```
 
 
+# Use qwx
+
+```bash
+qwx open -p <directory> # no homedir
+```
--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,10 @@ const HELP_CONTENT: &str = include_str!("../help.txt");
 fn cli() -> Command {
     Command::new(env!("CARGO_PKG_NAME"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
+        .version(env!("CARGO_PKG_VERSION"))
         .long_about(HELP_CONTENT)
         .subcommand(
-            Command::new("open").arg(
+            Command::new("open").about("Open a directory").arg(
                 Arg::new("path")
                     .short('p')
                     .required(false)
@@ -28,7 +29,14 @@ fn main() -> io::Result<()> {
     match matches.subcommand() {
         Some(("open", sub)) => {
             let p = sub.get_one::<String>("path").expect("required");
-            App::new(Path::new(p.as_str()))?.run()
+            if Path::new(p.as_str()).is_dir() {
+                App::new(Path::new(p.as_str()))?.run()
+            } else if dirs::home_dir().ne(&Some(Path::new(".").to_path_buf())) {
+                App::new(Path::new("."))?.run()
+            } else {
+                app.clone().print_long_help()?;
+                Ok(())
+            }
         }
         _ => {
             app.clone().print_long_help()?;
