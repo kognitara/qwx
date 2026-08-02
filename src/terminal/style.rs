2312da6c889494a7b0c966848b9f5159c7330e55
Shirusu Ryu <shirusuryu@gmail.com>
2026-08-02 03:36:14 +0200

    remove unseless code

--- src/terminal/style.rs
+++ src/terminal/style.rs
@@ -12,46 +12,6 @@ pub struct QwxStyle {
     pub attr: Option<Attribute>,
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Ord, PartialOrd, Hash)]
-pub enum QwxColor {
-    Reset,
-
-    Black,
-
-    DarkGrey,
-
-    Red,
-    DarkRed,
-
-    Green,
-
-    DarkGreen,
-
-    Yellow,
-
-    DarkYellow,
-
-    Blue,
-
-    DarkBlue,
-
-    Magenta,
-
-    DarkMagenta,
-
-    Cyan,
-
-    DarkCyan,
-
-    White,
-
-    Grey,
-
-    Rgb { r: u8, g: u8, b: u8 },
-
-    AnsiValue(u8),
-}
-
 #[derive(Clone, Copy)]
 pub struct QwxBorders {
     pub top_left: &'static str,
